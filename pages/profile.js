5e9121298c0500f3d799beeec1b079caa77623f1
kumastry <chma19063@g.nihon-u.ac.jp>
2023-01-27 23:32:41 +0900

    add signout dialog

--- pages/profile.js
+++ pages/profile.js
@@ -3,9 +3,22 @@ import { signOut } from "next-auth/react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Image from "next/image";
-
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from "react";
 export default function profile() {
   const { data: session, status } = useSession();
+  const [open, setOpen] = useState(false);
+  
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <main>
       {status === "authenticated" ? (
@@ -22,11 +35,28 @@ export default function profile() {
             <Button
               variant="contained"
               color="error"
-              onClick={() => signOut({ callbackUrl: "/" })}
+              onClick={handleClickOpen}
             >
               サインアウト
             </Button>
           </Box>
+
+          <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"本当にサインアウトしますか？"}
+        </DialogTitle>
+        <DialogActions>
+          <Button onClick={() => signOut({ callbackUrl: "/" })}>はい</Button>
+          <Button onClick={handleClose} autoFocus>
+            いいえ
+          </Button>
+        </DialogActions>
+      </Dialog>
         </div>
       ) : (
         <p>nasi</p>
