a1c9a85b62988daa3c2f47b0301e78a64b6f9ec5
kumastry <chma19063@g.nihon-u.ac.jp>
2023-04-30 16:53:26 +0900

    stagge

--- pages/api/boards/[boardId]/comments/index.js
+++ pages/api/boards/[boardId]/comments/index.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
       console.log("MEMEMETO")
       console.log(req.boardId);
       console.log("FD")
-      //const boardId = +req.params.boardId;
+      const boardId = +req.params.boardId;
       const limit = +req.query.limit || 50;
       const offset = +req.query.offset || 0;
       try {
