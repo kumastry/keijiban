843a13de9810e54c4715eecb46dbe285e2cfdf60
kumastry <chma19063@g.nihon-u.ac.jp>
2023-06-24 18:27:55 +0900

    typography atomの追加

---
+++ components/atoms/Typography.jsx
@@ -0,0 +1,19 @@
+import { Typography as MuiTypography } from "@mui/material";
+
+export default function Typography({
+    children,
+    variant,
+    align,
+    sx,
+    color,
+}) {
+    return (
+        <MuiTypography
+            variant={variant}
+            sx={sx}
+            color = {color}
+        >
+            {children}
+        </MuiTypography>
+    );
+}
